988a633481d0f708c7e7577d3b660dcca88460fe
Deskehs <124361319+Deskehs@users.noreply.github.com>
2025-09-30 00:42:30 +0800

    adjust vite

--- frontend/selector/vite.config.js
+++ frontend/selector/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
 
   build: {
-    outDir: '../../www_static/assets/selector',
+    outDir: "../../www_static/assets/selector",
 
     emptyOutDir: true,
-  }
-})
+
+    assetsDir: "",
+  },
+
+  base: "/assets/selector/",
+});
