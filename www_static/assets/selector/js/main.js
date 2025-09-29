7fd961ca446ca56ced2f337747274e472fa9bc3d
Deskehs <124361319+Deskehs@users.noreply.github.com>
2025-09-29 23:35:33 +0800

    giving up trying to make my own framework and use react instead

--- www_static/assets/selector/js/main.js
+++
@@ -1,8 +0,0 @@
-import { App } from "./app.js";
-
-document.addEventListener('DOMContentLoaded', async () => {
-    const appContainer = document.querySelector('.container')
-    const mainApp = new App(appContainer)
-
-    mainApp.render();
-});
