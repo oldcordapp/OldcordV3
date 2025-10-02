71cb5b49564b13bb99e6c42a26a3315992a02311
Deskehs <124361319+Deskehs@users.noreply.github.com>
2025-10-03 01:07:36 +0800

    update the page info

--- frontend/selector/src/components/views/settings/pluginsAndPatches.jsx
+++ frontend/selector/src/components/views/settings/pluginsAndPatches.jsx
@@ -8,12 +8,17 @@ export default function () {
     <>
       <Text variant="h1">Plugins & Patches</Text>
       <PageInfo title="Plugin & Patches Management">
-        Press the cog wheel or info icon to get more info on a plugin.
+        Press the cog wheel or info icon to get more info on a plugin or a
+        patch.
         <br />
-        Plugins with a cog wheel have settings you can modify!
+        Plugins and patches that has a cog wheel button have settings that you
+        can modify!
         <br />
         You can also select different enabled plugins and patches for each
         build.
+        <br />
+        WARNING: Some patches are not compatible with each other! You'll be
+        warned before saving.
       </PageInfo>
       <DropdownList
         label={"Client Build"}
