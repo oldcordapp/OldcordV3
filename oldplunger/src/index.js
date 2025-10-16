3cb1d5ec14c3549ab9d5e44452791ee6b1a0dc33
Deskehs <124361319+Deskehs@users.noreply.github.com>
2025-10-16 23:05:13 +0800

    adding some things to oldplunger for anyone who wants to contribute

--- oldplunger/src/index.js
+++ oldplunger/src/index.js
@@ -1,1 +1,9 @@
-console.log('[Oldplunger] Loaded!');
+// If it is possible to put `www_static/bootloader`'s AOT patching AND shimming into Oldplunger instead of deferring to bootloader it would be better.
+
+// Following Vencord's src/Vencord.ts
+
+async function init() {
+    console.log("[Oldplunger] Loaded!")
+}
+
+init();
---
+++ oldplunger/src/webpack/index.js
@@ -0,0 +1,6 @@
+/*
+ Following Vencord, I think this webpack folder does the following:
+ - Webpack Tools (find[Something])
+ - Patching by injecting to Webpack's module factory, converting to string, regex patch...? and then turn string back to a function
+ - Export React and other tools (useful for backporting the report modal, reply functionality and server banners)
+*/
