668adf7071e65d2291316c733144dfcc84a76ce9
Deskehs <124361319+Deskehs@users.noreply.github.com>
2025-10-17 12:04:48 +0800

    no more duplicate printing

--- oldplunger/src/webpack/webpack.js
+++ oldplunger/src/webpack/webpack.js
@@ -1,58 +1,61 @@
 export function webpack() {
-    // December 2018 (or maybe earlier) uses Webpack 4 which is an array which we can use modern methods (Moonlight/Vencord).
-    // For Webpack 3, in which webpackJsonp is a function, we have to use another way.
+  // December 2018 (or maybe earlier) uses Webpack 4 which is an array which we can use modern methods (Moonlight/Vencord).
+  // For Webpack 3, in which webpackJsonp is a function, we have to use another way.
 
-    let _webpackJsonp;
-    let originalWebpackJsonP;
-    let initialized = false;
-    window.oldplunger.webpackVersion = "3";
-    
-    Object.defineProperty(window, "webpackJsonp", {
-        set: (Jsonp) => {
-            if (initialized) {
-                _webpackJsonp = Jsonp;
-                return;
-            }
+  let _webpackJsonp;
+  let originalWebpackJsonP;
 
-            originalWebpackJsonP = Jsonp;
+  window.oldplunger.webpackVersion = "3";
 
-            if (typeof Jsonp == "function") {
-                console.log("[Webpack Patcher] Detected Webpack 3. Function wrapping is used.")
-                
-                _webpackJsonp = function(...args) {
-                    const modules = args[1];
+  Object.defineProperty(window, "webpackJsonp", {
+    set: (Jsonp) => {
+      if (Jsonp.oldplungerPatched) {
+        _webpackJsonp = Jsonp;
+        return;
+      }
 
-                    console.log(modules)
-                    
-                    if (modules) {
-                        console.log(modules);
-                    }
-                    return originalWebpackJsonP.apply(this, args);
-                }
+      originalWebpackJsonP = Jsonp;
 
-            } else if (Array.isArray(Jsonp)) {
-                window.oldplunger.webpackVersion = "4+";
-                console.log("[Webpack Patcher] Detected Webpack 4+. Modern patching is used.")
+      if (typeof Jsonp == "function") {
+        console.log(
+          "[Webpack Patcher] Detected Webpack 3. Function wrapping is used."
+        );
 
-                // Moonlight's method is used, would want to use Vencord's but I don't think it works for us.
-                // Thanks Moonlight!
+        _webpackJsonp = function (...args) {
+          const modules = args[1];
 
-                const originalPush = Jsonp.push;
+          console.log(modules);
 
-                if (Jsonp.push.oldplungerPatched !== true) {
-                    Jsonp.push = (items) => {
-                        console.log(items[1])
+          if (modules) {
+            console.log(modules);
+          }
+          return originalWebpackJsonP.apply(this, args);
+        };
 
-                        return originalPush.apply(originalWebpackJsonP, [items])
-                    }
-                    Jsonp.push.oldplungerPatched = true;
-                }
+        _webpackJsonp.oldplungerPatched = true;
+      } else if (Array.isArray(Jsonp)) {
+        window.oldplunger.webpackVersion = "4+";
+        console.log(
+          "[Webpack Patcher] Detected Webpack 4+. Modern patching is used."
+        );
 
-                _webpackJsonp = Jsonp
-            } else {
-                _webpackJsonp = Jsonp
-            }
-        },
-        get: () => _webpackJsonp
-    })
-};
+        // Moonlight's method is used, would want to use Vencord's but I don't think it works for us.
+        // Thanks Moonlight!
+
+        const originalPush = Jsonp.push;
+
+        Jsonp.push = (items) => {
+          console.log(items[1]);
+
+          return originalPush.apply(originalWebpackJsonP, [items]);
+        };
+        Jsonp.oldplungerPatched = true;
+
+        _webpackJsonp = Jsonp;
+      } else {
+        _webpackJsonp = Jsonp;
+      }
+    },
+    get: () => _webpackJsonp,
+  });
+}
