1e6537d38f9499c3036509440eeb1e27cb8ceafc
Deskehs <124361319+Deskehs@users.noreply.github.com>
2026-01-07 01:02:22 +0800

    PROPERLY use sapphire's snowflake because of Number concerns
    ohayo i tested in local it works

--- server/helpers/snowflake.js
+++ server/helpers/snowflake.js
@@ -1,12 +1,12 @@
 const cluster = require("cluster");
+const { DiscordSnowflake } = require("@sapphire/snowflake");
 
 // https://github.com/discordjs/discord.js/blob/master/src/util/Snowflake.js
 // Apache License Version 2.0 Copyright 2015 - 2021 Amish Shah
 // Stolen from fosscord, thanks
+// Modified to use @sapphire/snowflake as a compat layer because of bigint concerns
 
 class Snowflake {
-    static EPOCH = 1420070400000;
-    static INCREMENT = BigInt(0); // max 4095
     static processId = BigInt(process.pid % 31); // max 31
     static workerId = BigInt((cluster.worker?.id || 0) % 31); // max 31
 
@@ -14,64 +14,19 @@ class Snowflake {
         throw new Error(`The ${this.constructor.name} class may not be instantiated.`);
     }
 
-    static idToBinary(num) {
-        let bin = "";
-        let high = parseInt(num.slice(0, -10)) || 0;
-        let low = parseInt(num.slice(-10));
-        while (low > 0 || high > 0) {
-            bin = String(low & 1) + bin;
-            low = Math.floor(low / 2);
-            if (high > 0) {
-                low += 5000000000 * (high % 2);
-                high = Math.floor(high / 2);
-            }
-        }
-        return bin;
-    }
-
-    static binaryToID(num) {
-        let dec = "";
-
-        while (num.length > 50) {
-            const high = parseInt(num.slice(0, -32), 2);
-            const low = parseInt((high % 10).toString(2) + num.slice(-32), 2);
-
-            dec = (low % 10).toString() + dec;
-            num = Math.floor(high / 10).toString(2) +
-                Math.floor(low / 10).toString(2).padStart(32, "0");
-        }
-
-        num = parseInt(num, 2);
-        while (num > 0) {
-            dec = (num % 10).toString() + dec;
-            num = Math.floor(num / 10);
-        }
-
-        return dec;
-    }
-
-    static generateWorkerProcess() {
-        const time = BigInt(Date.now() - Snowflake.EPOCH) << BigInt(22);
-        const worker = Snowflake.workerId << 17n;
-        const process = Snowflake.processId << 12n;
-        const increment = Snowflake.INCREMENT++;
-        return BigInt(time | worker | process | increment);
-    }
 
     static generate() {
-        return Snowflake.generateWorkerProcess().toString();
+        return DiscordSnowflake.generate({processId: Snowflake.processId, workerId: Snowflake.workerId}).toString();
     }
 
     static deconstruct(snowflake) {
-        const BINARY = Snowflake.idToBinary(snowflake)
-            .toString(2)
-            .padStart(64, "0");
+        const deconstructed = DiscordSnowflake.deconstruct(snowflake);
         const res = {
-            timestamp: parseInt(BINARY.substring(0, 42), 2) + Snowflake.EPOCH,
-            workerID: parseInt(BINARY.substring(42, 47), 2),
-            processID: parseInt(BINARY.substring(47, 52), 2),
-            increment: parseInt(BINARY.substring(52, 64), 2),
-            binary: BINARY,
+            timestamp: Number(deconstructed.timestamp),
+            workerID: Number(deconstructed.workerId),
+            processID: Number(deconstructed.processId),
+            increment: Number(deconstructed.increment),
+            binary: BigInt(snowflake).toString(2).padStart(64, "0")
         };
         Object.defineProperty(res, "date", {
             get: function get() {
@@ -99,7 +54,7 @@ class Snowflake {
                 return false;
             }
 
-            if (timestamp < Snowflake.EPOCH) return false;
+            if (timestamp < DiscordSnowflake.epoch) return false;
 
             if (workerID < 0 || workerID > 31) return false;
             if (processID < 0 || processID > 31) return false;
