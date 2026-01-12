4389d61faea13adfcaa6c277caee1956a42ee6fd
Deskehs <124361319+Deskehs@users.noreply.github.com>
2026-01-12 23:58:10 +0800

    oops

--- server/types/modules/@spacebarchat/erlpack.d.ts
+++ server/types/modules/@spacebarchat/erlpack.d.ts
@@ -2,6 +2,6 @@ declare module '@spacebarchat/erlpack' {
   export function pack(data: any): Buffer;
   export function unpack<T = any>(
     data: Uint8Array | Uint8ClampedArray | Buffer,
-    decodeBigint = false,
+    decodeBigint: boolean,
   ): T;
 }
