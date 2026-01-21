129a8efdac72b2af16d7156844336764d246df65
Deskehs <124361319+Deskehs@users.noreply.github.com>
2026-01-21 09:47:09 +0800

    nvm we force people to not do shit code now

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,3 @@
-import { execSync } from 'child_process';
-
 export default {
   '*.{js,ts,jsx,tsx,mjs,mts,cjs,cts,json,jsonc,json5,md,css}': (filenames) => {
     const validFiles = filenames.filter((f) => !f.endsWith('package-lock.json'));
@@ -7,23 +5,7 @@ export default {
 
     const files = validFiles.map((f) => `"${f}"`).join(' ');
 
-    // We do not want to enforce people to fix their linter errors before committing yet during the transition period
-    // When we finally convert the whole project to TS, please replace the following with
-    // return [`eslint --fix ${files}`, `prettier --write ${files}`]
-    // to enforce fixing linting before committing
-    try {
-      execSync(`npx eslint --fix ${files}`, { stdio: 'inherit' });
-    } catch {
-      // If we don't use a try block, execSync will error for some reason.
-    }
-
-    try {
-      execSync(`npx prettier --write ${files}`, { stdio: 'inherit' });
-    } catch {
-      // Silently swallow errors
-    }
-
-    return [];
+    return [`eslint --fix ${files}`, `prettier --write ${files}`];
   },
   '*.{html,yml,yaml}': (filenames) => {
     return `prettier --write ${filenames}`;
