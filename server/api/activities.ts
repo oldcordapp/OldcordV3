1d47c6376d4f0e7a3e905990da6fc3308997bf06
ohayo <9443476+ohayo@users.noreply.github.com>
2026-02-11 10:49:14 +1000

    Update activities.ts

--- server/api/activities.ts
+++ server/api/activities.ts
@@ -1,9 +1,9 @@
+import type { Response } from 'express';
 import { Router } from 'express';
-import type { Response } from "express";
 
 const router = Router({ mergeParams: true });
 
-router.get('/statistics/applications/:applicationid', async (res: Response) => {
+router.get('/statistics/applications/:applicationid', (_req: any, res: Response) => {
   return res.status(200).json([]);
 });
 
