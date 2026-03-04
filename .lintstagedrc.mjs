16c9196408c62296d6feefd640e75e6d42ac86dc
Deskehs <124361319+Deskehs@users.noreply.github.com>
2026-03-05 03:10:33 +0800

    chore: move linting to biome

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,26 +1,10 @@
 export default {
-  '*.{ts,tsx,mts,cts,json,jsonc,json5,md}': (filenames) => {
+  '*.{js,jsx,mjs,cjs,ts,tsx,mts,cts,json,jsonc,json5,md,css}': (filenames) => {
     const validFiles = filenames.filter((f) => !f.endsWith('package-lock.json'));
     if (validFiles.length === 0) return [];
 
     const files = validFiles.map((f) => `"${f}"`).join(' ');
 
-    return [
-      `npx --node-options='--experimental-strip-types' eslint --flag unstable_native_nodejs_ts_config --fix ${files}`,
-      `prettier --write ${files}`,
-    ];
-  },
-  '*.css': (filenames) => {
-    const files = filenames.map((f) => `"${f}"`).join(' ');
-
-    return [
-      `npx --node-options='--experimental-strip-types' eslint --flag unstable_native_nodejs_ts_config --fix ${files}`,
-      `stylelint -o "stylelint.log" --fix ${files}`,
-      `prettier --write ${files}`,
-    ];
-  },
-  '*.{html,yml,yaml}': (filenames) => {
-    const files = filenames.map((f) => `"${f}"`).join(' ');
-    return `prettier --write ${files}`;
+    return [`npx @biomejs/biome check --write ${files}`];
   },
 };
